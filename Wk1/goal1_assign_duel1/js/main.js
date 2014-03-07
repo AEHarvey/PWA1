db9d0327d121c1dffcd7786bfa46d3f2df954425
Amy Harvey <AEHarvey@fullsail.edu>
2014-03-07 11:25:15 -0500

    Goal 1 The Duel Assignment
    These are changes made to goal 1 week 1 assignment the duel

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,25 @@
+/**
+ Name: Amy Harvey
+ Date: 3/5/14.
+ Assignemt: PWA1 Goal1: Assignment: Duel1
+ */
+
+//self-executing function
+(function(){
+
+  console.log("FIGHT!!!!")
+
+   //players names
+    var playeroneName = "Spiderman";
+    var playertwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //players health
+    var playeroneHealth = 100;
+    var playertwoHealth = 100;
+
+
+})();
